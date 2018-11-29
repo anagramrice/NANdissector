e7df5647ff5b54f34fc2eccf50195340c9661c30
HP <zhong@hp.com>
2018-11-29 15:00:00 -0800

    updating with cmdline ifc for file input

--- nanTrans.py
+++ nanTrans.py
@@ -1,6 +1,7 @@
 from scapy.all import *
 import traceback as tb
 import re
+import argparse
 
 actsubtype = ['Reserved',
 'Ranging Request',
@@ -66,6 +67,7 @@ def getsubtype(octet):
     for i in actionSubs:
         if "{:02x}".format(i[0]) == octet:
             return i[1]
+
 def parseNan(data):
     match = False
     for i in attrIDs:
@@ -84,31 +86,51 @@ def parseNan(data):
         #tb.print_exc()
         pass
 
-#nan = rdpcap('/home/hp/Desktop/nan/actiononly.pcapng')
-nan = rdpcap('/home/hp/Desktop/nan/pi-phoneFail-phonePublisher.pcapng')
-#nan = rdpcap('actiononly.pcapng')
-for packet in nan:
-    data = str(packet).encode('HEX')
-    if '09506f9a18' in data:
-        nandata = data.split('506f9a18')[1]
-        print 'action ', getsubtype(nandata[0:2])
-        parseNan(nandata[2:])
-    elif '09506f9a13' in data:
-        nandata = data.split('506f9a13')[1]
-        print 'serviceDis ' ,  nandata
-        parseNan(nandata)
-    elif '04506f9a18' in data:
-        nandata = data.split('506f9a18')[1]
-        print 'action ', getsubtype(nandata[0:2])
-        parseNan(nandata[2:])
-    elif '04506f9a13' in data:
-        nandata = data.split('506f9a13')[1]
-        print 'serviceDis ' ,  nandata
-        parseNan(nandata)
-    elif re.search('dd\w{2}506f9a13',data):
-        nandata = data.split('506f9a13')[1]
-        print 'Info Element ' ,  nandata
-        parseNan(nandata)
-    print '#'*20, 'end of packet'
+
+def binPackets(nan, isPcap):
+    for packet in nan:
+        if isPcap:
+            data = str(packet).encode('HEX')
+        else:
+            data = packet
+        if '09506f9a18' in data:
+            nandata = data.split('506f9a18')[1]
+            print 'action ', getsubtype(nandata[0:2])
+            parseNan(nandata[2:])
+        elif '09506f9a13' in data:
+            nandata = data.split('506f9a13')[1]
+            print 'serviceDis ' ,  nandata
+            parseNan(nandata)
+        elif '04506f9a18' in data:
+            nandata = data.split('506f9a18')[1]
+            print 'action ', getsubtype(nandata[0:2])
+            parseNan(nandata[2:])
+        elif '04506f9a13' in data:
+            nandata = data.split('506f9a13')[1]
+            print 'serviceDis ' ,  nandata
+            parseNan(nandata)
+        elif re.search('dd\w{2}506f9a13',data):
+            nandata = data.split('506f9a13')[1]
+            print 'Info Element ' ,  nandata
+            parseNan(nandata)
+        print '#'*20, 'end of packet'
 
 
+class Cmdparse(object):
+    def __init__(self):        
+        self.parser = argparse.ArgumentParser(prefix_chars="-")        
+        self.parser.add_argument("-f","-F","--fname",dest="fname", help="filename of either pcap hexdump of capture")
+        # Initialize printer variables
+        self._args = self.parser.parse_args()
+        print self._args.fname
+        if '.pcap' in self._args.fname[-7:]:
+            nan = rdpcap(self._args.fname)
+            isPcap = True
+        else:
+            nan = self._args.fname
+            isPcap = False
+        binPackets(nan, isPcap)
+        #nan = rdpcap('/home/hp/Desktop/nan/actiononly.pcapng')
+
+if __name__ == '__main__':
+    Cmdparse()
